a174e414983cf47ff7ea43b9fcc62157f4973e81
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-11-15 21:24:06 +0530

    style: pass tailwind classes as props in button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -14,7 +14,8 @@ const Button = (props: ButtonProps) => {
 			className={cn(
 				'flex items-center gap-2 rounded-lg px-4 py-2 text-sm duration-150 hover:bg-opacity-80',
 				props.variant === 'primary' && 'bg-primary text-white',
-				props.variant === 'secondary' && 'bg-secondary text-white'
+				props.variant === 'secondary' && 'bg-secondary text-white',
+				props.className
 			)}
 		>
 			{props.children}
