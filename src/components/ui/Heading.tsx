39eea1ab63e40cc29d9a8d683c82bf845e9cc027
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-11-15 19:03:21 +0530

    feat: add heading className prop
    fix: tsconfig paths not resolving

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,5 +1,16 @@
-const Heading = (props: { title: string }) => {
-	return <h2 className='text-2xl font-semibold'>{props.title}</h2>
+import { cn } from '@/utils/cn'
+
+type HeadingProps = {
+	title: string
+	className?: string
+}
+
+const Heading = (props: HeadingProps) => {
+	return (
+		<h2 className={cn('text-2xl font-semibold', props.className)}>
+			{props.title}
+		</h2>
+	)
 }
 
 export default Heading
