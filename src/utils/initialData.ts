f4edf34578dc7856ae606a5761714d7d7d65e592
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-11-15 14:44:35 +0530

    feat: add structured mockup data

---
+++ src/utils/initialData.ts
@@ -0,0 +1,32 @@
+export const initialData = {
+	profile: {
+		name: 'Adrian Brewer',
+		desc: 'Engineer at BB Agency - Industry',
+		location: 'New York, United States',
+		tags: ['UX Research', 'CX Strategy', 'Project Management'],
+	},
+	about:
+		"Hi, my name is Adrian Brewer, I'm the Co-founder and Head of Design at BB agency. Designer at heart. Head of Design might be an overstatement, but as with many 20 people agencies I need to wear many different hats...",
+	collaborates: ['Coco cola'],
+	products: [
+		{
+			name: 'Product #1',
+			price: 350,
+			currency: 'USD',
+			desc: '',
+		},
+		{
+			name: 'Product #2',
+			price: 350,
+			currency: 'USD',
+			desc: '',
+		},
+	],
+	testimonials: [
+		{
+			name: 'John smith',
+			company: 'Company',
+			desc: "Working with Jennifer was the best experience possible for us. We've had over 150 B2B partners come from her audience, and likely so many more influenced by her! Seriously, the best ROI we've seen.",
+		},
+	],
+}
