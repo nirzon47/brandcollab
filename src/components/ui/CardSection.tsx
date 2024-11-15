55e151bb4cf9a10db175d2a7b004563ecbc8eb78
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-11-15 19:45:13 +0530

    style: adjust padding for cards

--- src/components/ui/CardSection.tsx
+++ src/components/ui/CardSection.tsx
@@ -1,6 +1,6 @@
 const CardSection = (props: { children: React.ReactNode }) => {
 	return (
-		<section className='rounded-2xl bg-white px-8 py-4'>
+		<section className='rounded-2xl bg-white px-8 py-6'>
 			{props.children}
 		</section>
 	)
